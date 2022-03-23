0b80295914e1fb4c91debbf8bf0271278fc4c48b
balsac7 <70721744+balsac7@users.noreply.github.com>
2022-03-23 15:28:38 -0400

    added card flip to npm

--- server/server.js
+++ server/server.js
@@ -1,19 +1,16 @@
-const express = require("express");
+const path = require('path');
+const express = require('express');
 const app = express();
-const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
-app.use(cors());
-app.use(express.json());
-app.use(require("./routes/record"));
-// get driver connection
-const dbo = require("./db/conn");
- 
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- 
-  });
-  console.log(`Server is running on port: ${port}`);
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '..', 'public');
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
 });
+
+app.listen(port,() => {
+  console.log('Server is up!');
+});
+
