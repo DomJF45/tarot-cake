0227d8162ccb9bc362202006b72fe77d894a9ed0
balsac7 <70721744+balsac7@users.noreply.github.com>
2023-03-02 16:20:48 -0500

    started changes on register component, need to update login next

--- client/src/components/auth/Register.tsx
+++ client/src/components/auth/Register.tsx
@@ -1,15 +1,18 @@
 import React, { useState } from 'react'
 import { Button } from '../../styled-components/styles';
 import { Link } from 'react-router-dom';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import '../../styles/Auth.css'
 import { iUser } from '../../interfaces/user/user.interface';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 const Register = () => {
 
   const [userName, setUserName] = useState<string>('');
   const [userEmail, setUserEmail] = useState<string>('');
   const [userPassword, setUserPassword] = useState<string>('');
-  const [userData, setUserData] = useState<iUser | null>(null);
   const [showPass, setShowPass]: [boolean, (prev:boolean) => void] = useState<boolean>(false);
   
   const handleShowPass = (e: React.MouseEvent<HTMLButtonElement>): void => {
@@ -17,25 +20,43 @@ const Register = () => {
     setShowPass(!showPass);
   }
 
+  const handleRegister = (e: React.FormEvent) => {
+    e.preventDefault();
+
+   const userData = {
+      name: userName,
+      email: userEmail,
+      password: userPassword
+    }
+
+    console.log(userData);
+
+
+  } 
+
   return (
     <>
       <div className='auth-card'>
         <h1>Register</h1>
         <div className='form-container'>
-          <form className='auth-form'>
+          <form id='register-form' className='auth-form' onSubmit={handleRegister}>
             <div>
-              <input type='text' placeholder='Username' value={userData!.name} onChange={(e) => setUserName(e.currentTarget.value)} />
+              <input type='text' placeholder='Username' value={userName} onChange={(e) => setUserName(e.currentTarget.value)} />
             </div>
             <div>
-              <input type='text' placeholder='Email' value={userData!.name} onClick={(e) => setUserEmail(e.currentTarget.value)} />
+              <input type='text' placeholder='Email' value={userEmail} onChange={(e) => setUserEmail(e.currentTarget.value)} />
             </div>
-            <div style={{display: 'flex', justifyContent: 'center'}}>
-              <input type={showPass ? 'text' : 'password'} placeholder='Password' />
-              {/* <button onClick={(e): void => handleShowPass(e)}>Show Password?</button> */}
+            <div>
+              <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: '100%', gap: '10px', width: '100%' }}>
+                <input type={showPass ? 'text' : 'password'} placeholder='Password' value={userPassword} onChange={(e) => setUserPassword(e.currentTarget.value)} /> 
+              </div>  
+              { showPass ? <Visibility style={{cursor: 'pointer'}} onClick={() => setShowPass(!showPass)} fontSize='large' /> : <VisibilityOff style={{cursor: 'pointer'}} onClick={() => setShowPass(!showPass)} fontSize='large' /> }
             </div>
+            
+            
           </form>
         </div>
-        <Button>Register</Button>
+        <Button type="submit" form='register-form' >Register</Button>
         <div>
           <p>Already have an account? <Link to='/login'>Login</Link> </p>
         </div>
